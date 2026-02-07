f52309b86a04431be6f581aaf3e5f29aaa3f4162
Jemaimee <aurelienwontolla@gmail.com>
2026-02-07 11:38:39 +0100

    fixed zoom being stuck if zoomed out too much

--- camera.py
+++ camera.py
@@ -3,20 +3,24 @@ class Camera:
         self.cell_size = cell_size
         self.x = 0.0
         self.y = 0.0
-    
+
     def screen_to_cell(self, screen_x, screen_y):
         cell_x = (screen_x // self.cell_size) + self.x
         cell_y = (screen_y // self.cell_size) + self.y
         return cell_x, cell_y
+
     def cell_to_screen(self, cell_x, cell_y):
         screen_x = (cell_x - self.x) * self.cell_size
         screen_y = (cell_y - self.y) * self.cell_size
         return int(screen_x), int(screen_y)
+
     def zoom_in(self):
-        if self.cell_size > 1:
-            self.cell_size = int(self.cell_size / 1.5) 
+        if self.cell_size > 2:
+            self.cell_size = int(self.cell_size / 1.5)
+
     def zoom_out(self):
         self.cell_size = int(self.cell_size * 1.5)
+
     def move(self, x, y):
         self.x += x
         self.y += y
